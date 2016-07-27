69b9358416d101cd916ff8fbf695ceac4d498885
朱益达 <zhuxiaoyuan@10.10.10.21>
2016-07-27 18:47:34 +0800

    add: 系统通知

--- js-with-native/js-with-native/AppDelegate.m
+++ js-with-native/js-with-native/AppDelegate.m
@@ -15,6 +15,19 @@
 
 @implementation AppDelegate
 
+static void displayStatusChanged(CFNotificationCenterRef center,
+                                 void *observer,
+                                 CFStringRef name,
+                                 const void *object,
+                                 CFDictionaryRef userInfo) {
+    if (name == CFSTR("com.apple.springboard.lockcomplete")) {
+        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"kDisplayStatusLocked"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
+
+
+}
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
@@ -22,25 +35,71 @@
     LoginName *login = [[LoginName alloc] init ];
     self.window.rootViewController = login;
     [self.window makeKeyAndVisible];
+    
+    
+    CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(),
+                                    NULL,
+                                    displayStatusChanged,
+                                    CFSTR("com.apple.springboard.lockcomplete"),
+                                    NULL,
+                                    CFNotificationSuspensionBehaviorDeliverImmediately);
+   
+    
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    NSLog(@"我休息一下");
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
+    
+//    UIApplicationState state = [[UIApplication sharedApplication] applicationState];
+//    if (state == UIApplicationStateInactive) {
+//        NSLog(@"Sent to background by locking screen");
+//    } else if (state == UIApplicationStateBackground) {
+//        if (![[NSUserDefaults standardUserDefaults] boolForKey:@"kDisplayStatusLocked"]) {
+//            NSLog(@"Sent to background by home button/switching to other app");
+//        } else {
+//            NSLog(@"Sent to background by locking screen");
+//        }
+//    }
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    
+    if (![[NSUserDefaults standardUserDefaults] boolForKey:@"kDisplayStatusLocked"]) {
+        UIAlertController *alert1 = [UIAlertController alertControllerWithTitle:@"标题" message:@"开始工作了" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *action = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+            
+        }];
+        [alert1 addAction:action];
+        [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert1 animated:YES completion:nil];
+    }else{
+        UIAlertController *alert1 = [UIAlertController alertControllerWithTitle:@"标题" message:@"欢迎回来" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *action = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+            
+        }];
+        [alert1 addAction:action];
+        [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert1 animated:YES completion:nil];
+        
+    }
+    
+    [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"kDisplayStatusLocked"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    //
+
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
+
+
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
